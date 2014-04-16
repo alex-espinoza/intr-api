256af1c848337d5bd7b95425ba5a6e5068c694d9
Alex Espinoza <aespinoza@aspria.net>
2014-04-16 16:09:04 -0400

    Ensure authentication token when a user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :ensure_authentication_token
+
+private
+
+  def ensure_authentication_token
+    if authentication_token.blank?
+      self.authentication_token = generate_authentication_token
+    end
+  end
+
+  def reset_authentication_token
+    self.authentication_token = generate_authentication_token
+  end
+
+  def generate_authentication_token
+    loop do
+      token = SecureRandom.hex
+      break token unless User.find_by(authentication_token: token)
+    end
+  end
+
 end
