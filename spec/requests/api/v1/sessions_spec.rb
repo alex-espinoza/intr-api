31d48056dee4135f3195e5f753f61b58ff2e9ef7
Alex Espinoza <aespinoza@aspria.net>
2014-04-18 01:56:41 -0400

    Create Sessions API "POST /sessions" success test

---
+++ spec/requests/api/v1/sessions_spec.rb
@@ -0,0 +1,20 @@
+describe "Sessions API" do
+  describe "POST /sessions" do
+    it "on success, returns user's authentication token" do
+      user = FactoryGirl.create(:user)
+
+      user_params = {
+        "user" => {
+          "email" => user.email,
+          "password" => user.password
+        }
+      }.to_json
+
+      post "/sessions", user_params
+
+      expect(response.status).to eq(200)
+      json = JSON.parse(response.body)
+      expect(json['authentication_token']).to eq(user.authentication_token)
+    end
+  end
+end
