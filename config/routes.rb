0509ba4284aa0a78243e7bad7c42a3996de64e58
Alex Espinoza <aespinoza@aspria.net>
2014-04-21 17:20:53 -0400

    Add default devise_for :user routes but skip them

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 IntrApi::Application.routes.draw do
+  devise_for :users, skip: [:sessions, :passwords, :registrations]
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       devise_scope :user do
