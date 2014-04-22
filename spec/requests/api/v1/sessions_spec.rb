68be346c7ff4c03ce73820e7d5a0d7afda208e39
Alex Espinoza <aespinoza@aspria.net>
2014-04-22 15:38:27 -0400

    Create Sessions API "DELETE /sessions" test

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -37,4 +37,25 @@ describe API::V1::SessionsController do
       expect(json['message']).to eq("Invalid email and/or password.")
     end
   end
+
+  describe "DELETE /sessions" do
+    it "destroys the user's session" do
+      user = FactoryGirl.create(:user)
+
+      user_params = {
+        "user" => {
+          "email" => user.email,
+          "password" => user.password
+        }
+      }.to_json
+
+      post "/api/v1/sessions", user_params, request_headers
+
+      delete "/api/v1/sessions?authentication_token=#{user.authentication_token}", {}, request_headers
+
+      expect(response.status).to eq(200)
+      expect(json['success']).to be_true
+      expect(json['message']).to eq("You have been successfully logged out.")
+    end
+  end
 end
