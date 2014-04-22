e558092673f0602313aa96caa8f57decdf9ea2a8
Alex Espinoza <aespinoza@aspria.net>
2014-04-21 22:21:36 -0400

    Create Sessions API "POST /sessions" failure test

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -25,5 +25,28 @@ describe API::V1::SessionsController do
       expect(json['message']).to eq("You have been successfully logged in.")
       expect(json['data']['authentication_token']).to eq(user.authentication_token)
     end
+
+    it "on failure, returns error message" do
+      user = FactoryGirl.create(:user)
+
+      user_params = {
+        "user" => {
+          "email" => user.email,
+          "password" => 'nottherightpass'
+        }
+      }.to_json
+
+      request_headers = {
+        "Accept" => "application/json",
+        "Content-Type" => "application/json"
+      }
+
+      post "/api/v1/sessions", user_params, request_headers
+
+      expect(response.status).to eq(401)
+      json = JSON.parse(response.body)
+      expect(json['success']).to be_false
+      expect(json['message']).to eq("Invalid email and/or password.")
+    end
   end
 end
