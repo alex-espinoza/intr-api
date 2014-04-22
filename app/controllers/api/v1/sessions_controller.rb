79d4241e264ced9e04a66eecc3c7f62889152775
Alex Espinoza <aespinoza@aspria.net>
2014-04-21 22:20:48 -0400

    Created create action in API:V1:SessionsController, spec test passing

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,5 +5,10 @@ class API::V1::SessionsController < Devise::SessionsController
   respond_to :json
 
   def create
+    warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+    render :status => 200,
+             :json => { :success => true,
+                        :message => "You have been successfully logged in.",
+                           :data => { :authentication_token => current_user.authentication_token } }
   end
 end
