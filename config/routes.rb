fbb61443aff0aad756d3cd2dda4d926c47cff706
Alex Espinoza <aespinoza@aspria.net>
2014-04-22 15:39:00 -0400

    Create DELETE 'api/v1/sessions' route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ IntrApi::Application.routes.draw do
     namespace :v1 do
       devise_scope :user do
         post 'sessions' => 'sessions#create', :as => 'login'
+        delete 'sessions' => 'sessions#destroy', :as => 'logout'
       end
     end
   end
