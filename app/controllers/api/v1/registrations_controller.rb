0c22b23900da95c5349dcce09534181d4b70d8dc
Alex Espinoza <aespinoza@aspria.net>
2014-04-23 13:44:47 -0400

    Created 'create' action for API::V1::RegistrationsController, tests passing

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,2 +1,22 @@
-class API::V1::RegistrationsController < API::V1::ApiController
+class API::V1::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  def create
+    build_resource(user_params)
+    if resource.save
+      return render :status => 200,
+                      :json => { :success => true,
+                                 :message => "New account has been successfully created.",
+                                    :data => { :user => { :id => resource.id,
+                                                          :email => resource.email,
+                                                          :authentication_token => resource.authentication_token,
+                                                          :created_at => resource.created_at } } }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
