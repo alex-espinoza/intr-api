1f1edddefa5bc1400592e9ea22c76b20ff5415e7
Alex Espinoza <aespinoza@aspria.net>
2014-04-23 13:11:39 -0400

    Add POST "/registrations" route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ IntrApi::Application.routes.draw do
       devise_scope :user do
         post 'sessions' => 'sessions#create', :as => 'login'
         delete 'sessions' => 'sessions#destroy', :as => 'logout'
+        post 'registrations' => 'registrations#create', :as => 'register'
       end
     end
   end
