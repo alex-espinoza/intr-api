7eaada7ab6013bae8bffc2ce58b32cc1ea75c3f0
Alex Espinoza <aespinoza@aspria.net>
2014-04-23 14:10:31 -0400

    Create Registrations API "POST /registrations" failure test

--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -25,5 +25,23 @@ describe API::V1::RegistrationsController do
       expect(json['data']['user']['authentication_token']).to eq(new_user.authentication_token)
       expect(json['data']['user']['created_at']).to eq(new_user.created_at.as_json)
     end
+
+    it "on failure, does not create a new user" do
+      user = FactoryGirl.build(:user)
+
+      user_params = {
+                      "user" => {
+                        "email" => user.email,
+                        "password" => 'thispass',
+                        "password_confirmation" => 'doesnotmatch'
+                      }
+                    }.to_json
+
+      post "/api/v1/registrations", user_params, request_headers
+
+      expect(response.status).to eq(422)
+      expect(json['success']).to be_false
+      expect(json['message']).to eq("Errors")
+    end
   end
 end
