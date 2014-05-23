f347cffab4cf710e8c1c221278a6723905fa6d87
Alex Espinoza <aespinoza@aspria.net>
2014-05-22 21:06:29 -0400

    Create test users resource to try out API

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ IntrApi::Application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :users
       devise_scope :user do
         post 'sessions' => 'sessions#create', :as => 'login'
         delete 'sessions' => 'sessions#destroy', :as => 'logout'
